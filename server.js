df5eb851d6f4076bd2c8690eef0bc2ca52f4c708
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-14 15:07:28 -0700

    added whiteboard layout

--- server.js
+++ server.js
@@ -11,8 +11,26 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
+//sending to routes
 
+//home route
 app.use('/',homeRoutes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(process.env.PORT, ()=>{
     console.log(`Your server is running on port: ${process.env.PORT}`)
 })
