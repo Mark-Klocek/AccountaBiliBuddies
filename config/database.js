2e955e97eb91ee931864d7aae8a9fb19338bb159
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-16 16:26:27 -0700

    added all files required for project, updated passport config, connected to passport, and finalized all modules in the server.js file

--- config/database.js
+++ config/database.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose'
 
 const connectDB = async () =>{
     try{
-        const conn = await mongoose.connect(process.env.DB_STRING)
+        const conn = await mongoose.connect(process.env.DB_STRING);
+        console.log(`MongoDB connected: ${conn.connection.host}`)
     }catch(err){
         console.error(err)
         process.exit(1)
     }
 }
+
+export default connectDB
