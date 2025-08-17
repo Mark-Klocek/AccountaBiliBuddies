2b65cddf8785a0d264844aedefdd4424d885d367
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-17 15:12:57 -0700

    fixed a couple of routing / views issues with Tomorrow, also had ejs start to read parameters from the database to populate username, highest streak and lowest streak

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,8 +1,15 @@
-
+import User from '../models/User.js'
 class profileController {
 
-    getProfile (req,res){
-        res.render('profile.ejs');
+    async getProfile  (req,res){
+        try{
+            const userInfo = await User.findById(req.user._id)
+            console.log(userInfo)
+            res.render('profile.ejs', {user: userInfo});
+        }catch(err){
+            console.log(err)
+        }
+        
     }
     
 }
