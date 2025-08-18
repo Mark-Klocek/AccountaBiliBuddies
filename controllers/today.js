955709ae1b98462bf8dc4a8b04bdae3292bc24a4
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-18 16:12:53 -0700

    current streak and highest streak are now working

--- controllers/today.js
+++ controllers/today.js
@@ -13,10 +13,47 @@ class todayController {
             const user = await User.findById(req.user._id)
             const today = await Day.findOne({ownerID : req.user._id, effectiveDate}).lean()
 
+            // console.log(user)
             console.log(user)
-            console.log(today)
             
             res.render('today.ejs',{user,today});
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    async updateTask (req,res){
+        try {
+            const today = await Day.findOneAndUpdate(
+                                        { _id: req.body.dayId, "dailyTasks._id": req.body.taskId },
+                                        { 
+                                            $set: { "dailyTasks.$.isComplete": true },
+                                            $inc: {completedTasks: 1} 
+                                        },
+                                        
+                                        { new: true }
+                                        )
+            if(today.completedTasks == today.dailyTasks.length){
+                const u = await User.findByIdAndUpdate(
+                    {
+                        _id:req.user._id
+                    },
+                    {
+                        $inc:{currentStreak: 1}
+                    },
+                    {new:true})
+                if (u && u.currentStreak > u.highestStreak){
+                    await User.findByIdAndUpdate(
+                        {
+                            _id:req.user._id
+                        },
+                        {
+                            $set:{highestStreak: u.currentStreak}
+                        },
+                        {new:true}
+                    )
+                }
+            }
+            res.redirect('/today')
         } catch (error) {
             console.log(error)
         }
